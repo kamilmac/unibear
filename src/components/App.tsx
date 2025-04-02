7b128cee26561cf3902904a5c094577f19410a29
kamilmac <kmacinski@gmail.com>
2025-04-02 11:24:31 +0200

    +

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,5 @@
 import React from "npm:react";
-import { Box, Text } from "npm:ink";
+import { Box, measureElement, Text, useInput } from "npm:ink";
 import { useStore } from "../store/index.ts";
 import { UserInput } from "./UserInput.tsx";
 
@@ -23,13 +23,17 @@ export const App = () => {
         borderStyle="round"
         height={dims.rows - 6}
       >
-        {chat.map((item, index) => (
-          <Box
-            key={index}
-          >
-            <Text>{item.contentOverride ?? item.content}</Text>
-          </Box>
-        ))}
+        <ScrollArea height={dims.rows - 6}>
+          {chat.map((item, index) => (
+            <Box
+              key={index}
+              width={dims.cols - 2}
+              flexShrink={0}
+            >
+              <Text>{item.contentOverride ?? item.content}</Text>
+            </Box>
+          ))}
+        </ScrollArea>
       </Box>
       <Box borderStyle="round" height={6}>
         <UserInput />
@@ -37,3 +41,48 @@ export const App = () => {
     </Box>
   );
 };
+
+function ScrollArea(
+  { height, children }: { height: number; children?: React.Element },
+) {
+  const [scrollTop, setScrollTop] = React.useState(0);
+
+  const innerRef = React.useRef();
+
+  React.useEffect(() => {
+    const dimensions = measureElement(innerRef.current);
+
+    console.log(dimensions.height);
+    // dispatch({
+    //   type: "SET_INNER_HEIGHT",
+    //   innerHeight: dimensions.height,
+    // });
+  }, []);
+
+  useInput((_input, key) => {
+    if (key.downArrow) {
+      setScrollTop(scrollTop - 1);
+    }
+
+    if (key.upArrow) {
+      setScrollTop(scrollTop + 1);
+    }
+  });
+
+  return (
+    <Box
+      height={height}
+      flexDirection="column"
+      overflow="hidden"
+    >
+      <Box
+        ref={innerRef}
+        flexShrink={0}
+        flexDirection="column"
+        marginTop={-scrollTop}
+      >
+        {children}
+      </Box>
+    </Box>
+  );
+}
